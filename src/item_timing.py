2bdfab8652eaa2bb1fd34e517c71d19dc692355e
Alex Kindel <atkindel@stanford.edu>
2016-01-19 17:11:08 -0800

    Switch item_timing implementation to mutable_tree data type

--- src/item_timing.py
+++ src/item_timing.py
@@ -1,6 +1,11 @@
 ## Script to transform EventXtract table events to learner-problem pair timing data
 ## Author: Alex Kindel
 ## Date: 12 November 2015
+#
+# Changelog
+#   [19 Jan 16]: Use MutableTree data type to store calculations.
+#
+#
 
 import csv
 import json
@@ -9,61 +14,34 @@ import datetime
 import random
 from os.path import expanduser
 from itertools import izip
-from collections import namedtuple, defaultdict, MutableMapping
-
-# Data type for calculations. Behaves like a dictionary, but with an immutable key set.
-class Calculations(MutableMapping):
-    '''
-    Data type defines a dictionary with a set of allowed keys.
-    '''
-    # Class variable that circumscribes allowed calculation keys
-    CALCS = ['first_attempt',
-             'last_attempt',
-             'last_grade',
-             'time_to_first_attempt',
-             'time_to_last_attempt',
-             'time_spent_attempting',
-             'n_attempts',
-             'first_view'
-            ]
-
-    def __init__(self):
-        self.data = dict.fromkeys(Calculations.CALCS)
-
-    # Decorator to check key on setter method
-    def restrict_key(func):
-        def check(self, key, value):
-            if key in Calculations.CALCS:
-                return func(self, key, value)
-            else:
-                raise KeyError("Key '%s' not in list of permitted keys." % key)
-        return check
-
-    def __getitem__(self, key):
-        return self.data[key]
-
-    @restrict_key
-    def __setitem__(self, key, value):
-        self.data[key] = value
-
-    def __delitem__(self, key):
-        del self.data[key]
-
-    def __iter__(self):
-        return iter(self.data)
-
-    def __len__(self):
-        return len(self.data)
+from mutable_tree import MutableTree
+from collections import namedtuple, defaultdict
 
 
-# JSON encoder subclass handles our Calculations data type
-class CalcEncoder(json.JSONEncoder):
-    def default(self, o):
-        return o.data
+# Data type for calculations.
+class Calculations(MutableTree):
+    keylist = ['first_attempt',
+        'last_attempt',
+        'n_attempts',
+        'last_grade',
+        'time_to_first_attempt',
+        'time_to_last_attempt',
+        'time_spent_attempting',
+        'first_view'
+    ]
 
+# Container class with unrestricted tree semantics
+class Tree(MutableTree):
+    keylist = None
+
+# JSON encoder subclass for serializing our calculations data type
+class MTreeEncoder(json.JSONEncoder):
+    def default(self, obj): return obj.data
+
+# JSON encoder subclass for serializing datetime data
 class DateEncoder(json.JSONEncoder):
-    def default(self, o):
-        return str(o)
+    def default(self, obj): return str(obj)
+
 
 
 class ItemTimingComputer(object):
@@ -84,15 +62,19 @@ class ItemTimingComputer(object):
         self.problem_events = home + problem_events_dir
         self.browse_events = home + browse_events_dir
 
-        # Also store a couple of data structures to store data and computations.
-        self.events = self.tree(list)
-        self.responses = self.tree(list)
-        self.computations = self.tree(Calculations)
+        # Parsed course event data ###TODO: Use epoch time
+        self.events = Tree()
+        self.responses = Tree()
         self.registrations = defaultdict(list)
         self.problem_meta = defaultdict(list)
-        self.aggregate = self.stats()
-        self.missing = self.stats()
-        self.ignored = self.stats()
+
+        # Aggregate descriptive statistics
+        self.aggregate = defaultdict(int)
+        self.missing = defaultdict(int)
+        self.ignored = defaultdict(int)
+
+        # Computed data
+        self.computations = Calculations()
 
         # Are we built yet? Have we computed yet?
         self.built = False
@@ -100,15 +82,6 @@ class ItemTimingComputer(object):
 
         # Finally, we want to keep track of all the problems we run into.
         self.problemset = set()
-
-    # TODO: This could have a better interface if it were its own data type
-    # Also try doing def tree(): return defaultdict(tree)-- this lets you be less explicit about leaf type
-    @staticmethod
-    def tree(ddtype):
-        '''
-        Use defaultdicts to simulate a tree of depth 2 with leaves of type 'ddtype'
-        '''
-        return defaultdict(lambda: defaultdict(ddtype))
 
     @staticmethod
     def splitn(obj, char, idx):
@@ -118,23 +91,16 @@ class ItemTimingComputer(object):
         return obj.split(char)[idx]
 
     @staticmethod
-    def stats():
-        '''
-        We want to accumulate a dictionary of counts for each of these events
-        '''
-        return {'problem_check': 0, 'problem_show': 0, 'problem_reset': 0, 'problem_save': 0, 'problem_check_fail': 0}
-
-    @staticmethod
     def extractProblemID(event_type, raw_id):
         '''
         Extract problem ID from event. Requires a little dark magic.
         '''
         problemID = None
         if event_type == 'problem_show':
-            problemID = raw_id.splitn('/', 5)
+            problemID = raw_id.split('/')[5]
         elif event_type in ['problem_reset', 'problem_check', 'problem_save']:
             raw_id = raw_id.replace('://', '/').replace('/', '-')
-            problemID = raw_id.splitn('-', 4).splitn('_', 0)
+            problemID = raw_id.split('-')[4].split('_')[0]
         else:
             print "Warning: uncaught event_type %s" % event_type
         return problemID
@@ -148,41 +114,42 @@ class ItemTimingComputer(object):
         else:
             with open(self.problem_events, 'rU') as f:
                 raw_events = csv.DictReader(f)
+                RawEvent = namedtuple('raw_event', ['learner', 'item', 'type', 'source', 'grade', 'page', 'rdn', 'time'])
                 for row in raw_events:
-                    # Get data
-                    learnerID = row['anon_screen_name']
-                    raw_problemID = row['problem_id']
-                    event_type = row['event_type']
-                    source = row['event_source']
-                    grade = row['success']
-                    page = row['page']
-                    rdn = row['resource_display_name']
-                    timing = datetime.datetime.fromtimestamp(time.mktime(time.strptime(row['time'], '%m/%d/%y %H:%M:%S')))
-                    #timing = time.strptime(row['time'], '%m/%d/%y %H:%M:%S')
+
+                    # Read data to namedtuple
+                    event = RawEvent(learner=row['anon_screen_name'],
+                        item=row['problem_id'],
+                        type=row['event_type'],
+                        source=row['event_source'],
+                        grade=row['success'],
+                        page=row['page'],
+                        rdn=row['resource_display_name'],
+                        time=(datetime.datetime.fromtimestamp(time.mktime(time.strptime(row['time'], '%m/%d/%y %H:%M:%S')))-datetime.datetime(1970,1,1)).total_seconds()
+                        )
 
                     # Skip if we didn't get any problemID information (might be '\N' or empty string)
-                    if len(raw_problemID) < 3:
-                        self.missing[event_type] += 1 # keep track of what we're losing
+                    if len(event.item) < 3:
+                        self.missing[event.type] += 1 # keep track of what we're losing
                         continue
 
                     # Drop events that aren't problem_checks
-                    if event_type in ['problem_reset', 'problem_save', 'problem_check_fail']:
-                        self.ignored[event_type] += 1
+                    if event.type in ['problem_reset', 'problem_save', 'problem_check_fail']:
+                        self.ignored[event.type] += 1
                         continue
 
-                    # Clean up problemID data by extracting identifier; log in problem set
-                    problemID = self.extractProblemID(event_type, raw_problemID)
+                    # Otherwise, store aggregate count data
+                    self.aggregate[event.type] += 1
+
+                    # Clean up problemID data by extracting identifier and log in problem set
+                    problemID = self.extractProblemID(event.type, event.item)
                     self.problemset.add(problemID)
 
-                    # Store aggregate count data
-                    self.aggregate[event_type] += 1
-
-                    # Store specific event data
-                    parsed_event = (timing, event_type, page, rdn, source, grade)
-                    if source == 'browser':
-                        self.events[learnerID][problemID].append(parsed_event)
+                    # Store event data
+                    if event.source == 'browser':
+                        self.events[event.learner][problemID] = event
                     else:
-                        self.responses[learnerID][problemID].append(parsed_event)
+                        self.responses[event.learner][problemID] = event
 
             self.built = True
 
@@ -208,55 +175,49 @@ class ItemTimingComputer(object):
         else:
             # First, we'll sort and organize our problem interaction data
             for learner in self.events:
-                for problem in self.events[learner]:
+                for item in self.events[learner]:
                     # First, we get our event list
-                    event_list = sorted(self.events[learner][problem])
+                    event_list = sorted(self.events[learner][item], key=lambda x: x.time)
 
                     # Next, we can fetch our matching list of server responses
-                    response_list = sorted(self.responses[learner][problem])
+                    response_list = sorted(self.responses[learner][item], key=lambda x: x.time)
 
                     # Then, we want each column of the event data separately
-                    times, types, pages, rdns, sources, grades = [list(elem) for elem in zip(*event_list)]
+                    _, _, types, sources, _, pages, rdns, _ = [list(elem) for elem in zip(*event_list)]
+                    try:
+                        _, _, _, _, grades, _, _, times = [list(elem) for elem in zip(*response_list)]
+                    except:
+                        grades = ['none']
 
-                    # Store problem metadata
-                    self.problem_meta[problem] = [pages[0], rdns[0]]
+                    # Store item metadata
+                    self.problem_meta[item] = [pages[0], rdns[0]]
 
                     # Compute and store in our data structures as defined above
-                    # Leave these calculable for the time being
-                    data = Calculations()
-                    data['first_attempt'] = times[0]
-                    data['last_attempt'] = times[-1]
-                    data['n_attempts'] = len(times)
-                    data['last_grade'] = grades[-1]
-                    self.computations[learner][problem] = data
+                    self.computations[learner][item]['first_attempt'] = times[0]
+                    self.computations[learner][item]['last_attempt'] = times[-1]
+                    self.computations[learner][item]['time_spent_attempting'] = times[-1] - times[0]
+                    self.computations[learner][item]['n_attempts'] = len(times)
+                    self.computations[learner][item]['last_grade'] = grades[-1]
 
             # Now we'll run through our browsing data and do our calculations
             with open(self.browse_events, 'rU') as f:
                 events = csv.DictReader(f)
                 for row in events:
                     learner = row['anon_screen_name']
-                    etype_str = row['event_type']
-                    if 'problem_get' not in etype_str:
+                    etype = row['event_type']
+                    if 'problem_get' not in etype:
                         continue
-                    problem = etype_str.splitn('/', 6).split(';', 5).strip('_')
-                    timing = datetime.datetime.fromtimestamp(time.mktime(time.strptime(row['time'], '%m/%d/%y %H:%M:%S')))
+                    problem = etype.split('/')[6].split(';')[5].strip('_')
+                    timing = (datetime.datetime.fromtimestamp(time.mktime(time.strptime(row['time'], '%m/%d/%y %H:%M:%S')))-datetime.datetime(1970,1,1)).total_seconds()
 
                     # Replace if our new time is lower for this pair
-                    if learner not in self.computations or problem not in self.computations[learner]:
+                    if learner not in self.computations.keys() or problem not in self.computations[learner].keys():
                         continue # We'll skip this row if the learner never tried the problem.
-                    vals = self.computations[learner][problem]
-                    if vals['first_view'] == None:
+                    if 'first_view' not in self.computations[learner][problem].keys():
                         # This value of timing is our first_view value. Do computations:
-                        first_try = vals['first_attempt']
-                        last_try = vals['last_attempt']
-                        self.computations[learner][problem]['time_to_last_attempt'] = (last_try - timing).seconds
-                        self.computations[learner][problem]['time_to_first_attempt'] = (first_try - timing).seconds
-                        self.computations[learner][problem]['time_spent_attempting'] = (last_try - first_try).seconds
-
-                        # We'll serialize our timestamps before we put them back
-                        self.computations[learner][problem]['first_attempt'] = str(first_try)
-                        self.computations[learner][problem]['last_attempt'] = str(last_try)
-                        self.computations[learner][problem]['first_view'] = str(timing)
+                        self.computations[learner][problem]['first_view'] = timing
+                        self.computations[learner][problem]['time_to_last_attempt'] = self.computations[learner][problem]['last_attempt'][0] - timing
+                        self.computations[learner][problem]['time_to_first_attempt'] = self.computations[learner][problem]['first_attempt'][0] - timing
 
     def check(self):
         '''
@@ -264,9 +225,9 @@ class ItemTimingComputer(object):
         '''
         learner = random.choice(self.computations.keys())
         print learner
-        print json.dumps(self.events[learner], indent=4, cls=DateEncoder)
-        print json.dumps(self.responses[learner], indent=4, cls=DateEncoder)
-        print json.dumps(self.computations[learner], indent=4, cls=CalcEncoder)
+        print json.dumps(self.events[learner], indent=4, cls=MTreeEncoder)
+        print json.dumps(self.responses[learner], indent=4, cls=MTreeEncoder)
+        print json.dumps(self.computations[learner], indent=4, cls=MTreeEncoder)
 
     def summary(self):
         '''
@@ -297,7 +258,7 @@ class ItemTimingComputer(object):
         '''
         Given an outfile path, write the desired calculation to CSV.
         '''
-        if calc not in Calculations().CALCS:
+        if calc not in Calculations().keylist:
             raise KeyError("Calculation %s not valid." % calc)
         else:
             with open(outfile, 'w') as out:
@@ -308,14 +269,14 @@ class ItemTimingComputer(object):
                 for learner in self.computations:
                     rowdata = {'learner': learner}
                     for problem in self.computations[learner]:
-                        rowdata[problem] = self.computations[learner][problem][calc]
+                        rowdata[problem] = self.computations[learner][problem][calc][0]
                     wrt.writerow(rowdata)
 
 
 
 if __name__ == '__main__':
     # Set up our event timing computer
-    problem_events_dir = "/Code/irt/data/ProblemEvents.csv" #TODO: this should track server problem_check responses too
+    problem_events_dir = "/Code/irt/data/ProblemEvents.csv"
     browse_events_dir = "/Code/irt/data/BrowseEvents.csv"
     timer = ItemTimingComputer(problem_events_dir, browse_events_dir)
 
@@ -324,11 +285,12 @@ if __name__ == '__main__':
     timer.compute()
 
     # Check some learner's data
-    timer.check()
+    #timer.check()
 
     # Output summary counts
-    timer.summary()
+    #timer.summary()
 
-    # Write out some data to CSV
-    # timer.writeCSV('time_to_last_attempt', '/Users/vpoluser/VPTL/IRT/time_to_last_attempt.csv')
-    # timer.writeProblemMeta('/Users/vpoluser/VPTL/IRT/problem_metadata.csv')
+    # Write out data to CSV
+    for calc in Calculations().keylist:
+        timer.writeCSV('%s'%calc, '/Users/vpoluser/Code/irt/data/exports/%s.csv'%calc)
+    timer.writeProblemMeta('/Users/vpoluser/Code/irt/data/exports/problem_metadata.csv')
