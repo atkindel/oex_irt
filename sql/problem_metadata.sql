5c898c8887b492d4e4a56af50406af0f004836b0
Alex Kindel <atkindel@stanford.edu>
2016-04-29 13:40:06 -0700

    Escape problem text field

--- sql/problem_metadata.sql
+++ sql/problem_metadata.sql
@@ -2,7 +2,7 @@ SELECT 'problem_id', 'problem_display_name', 'course_display_name', 'problem_tex
 UNION ALL
 SELECT ep.problem_id, ep.problem_display_name, ep.course_display_name, ep.problem_text, ep.trackevent_hook, ep.vertical_uri, ep.problem_idx, ep.sequential_uri, ep.vertical_idx
 INTO OUTFILE '/home/dataman/Data/CustomExcerpts/SU_Kindel_IRT_raws/{0}_ProblemMetadata.csv'
-	FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+	FIELDS ESCAPED BY '"' TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 	LINES TERMINATED BY '\n'
 FROM Edx.EdxProblem ep
 WHERE course_display_name = '{1}'
