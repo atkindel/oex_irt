29bde7fcff847ad349d084d36f314b932bf1cc5a
Alex Kindel <atkindel@stanford.edu>
2016-05-04 13:35:12 -0700

    Add enrollment time deltas to registrations table

--- sql/perlearner_first_event.sql
+++ sql/perlearner_first_event.sql
@@ -1,10 +1,12 @@
-SELECT 'time', 'anon_screen_name', 'event_type'
+SELECT 'time', 'anon_screen_name', 'event_type', 'course_start', 'start_delta', 'course_end', 'end_delta'
 UNION ALL
-SELECT min(`time`), anon_screen_name, event_type
+SELECT min(et.`time`), et.anon_screen_name, et.event_type, ci.start_date as course_start, (unix_timestamp(et.`time`) - unix_timestamp(ci.start_date)) as start_delta, ci.end_date as course_end, (unix_timestamp(et.`time`) - unix_timestamp(ci.end_date)) as end_delta
 INTO OUTFILE '/home/dataman/Data/CustomExcerpts/SU_Kindel_IRT_raws/{0}_Registrations.csv'
 	FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 	LINES TERMINATED BY '\n'
-FROM Edx.EdxTrackEvent
-WHERE course_display_name = '{1}'
+FROM Edx.EdxTrackEvent et, Edx.CourseInfo ci
+WHERE et.course_display_name = '{1}'
+AND et.course_display_name = ci.course_display_name
 AND event_type = 'edx.course.enrollment.activated'
-GROUP BY anon_screen_name;
+GROUP BY anon_screen_name
+ORDER BY `time`;
