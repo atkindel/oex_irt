43aea23f82c9feb32805e19c07991fceab9e228e
Alex Kindel <atkindel@stanford.edu>
2016-05-04 13:38:15 -0700

    Add course context data up to chapter level

--- sql/problem_metadata.sql
+++ sql/problem_metadata.sql
@@ -1,6 +1,8 @@
-SELECT 'problem_id', 'problem_display_name', 'course_display_name', 'problem_text', 'trackevent_hook', 'vertical_uri', 'problem_idx', 'sequential_uri', 'vertical_idx'
+SELECT 'problem_id', 'problem_display_name', 'course_display_name', 'problem_text', 'trackevent_hook',
+ 'vertical_uri', 'problem_idx', 'sequential_uri', 'vertical_idx', 'chapter_uri', 'sequential_idx', 'chapter_idx'
 UNION ALL
-SELECT ep.problem_id, ep.problem_display_name, ep.course_display_name, ep.problem_text, ep.trackevent_hook, ep.vertical_uri, ep.problem_idx, ep.sequential_uri, ep.vertical_idx
+SELECT ep.problem_id, ep.problem_display_name, ep.course_display_name, ep.problem_text, ep.trackevent_hook,
+ ep.vertical_uri, ep.problem_idx, ep.sequential_uri, ep.vertical_idx, ep.chapter_uri, ep.sequential_idx, ep.chapter_idx
 INTO OUTFILE '/home/dataman/Data/CustomExcerpts/SU_Kindel_IRT_raws/{0}_ProblemMetadata.csv'
 	FIELDS ESCAPED BY '"' TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 	LINES TERMINATED BY '\n'
