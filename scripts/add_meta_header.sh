294297c7cc7d946e89b8572b2ff1e48fa71eb8e0
Alex Kindel <atkindel@stanford.edu>
2016-05-09 21:34:09 -0700

    Adjust meta header

--- scripts/add_meta_header.sh
+++ scripts/add_meta_header.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 for file in ~/Code/irt/data/raws/*_ProblemMetadata_headless.csv; do
   outf=$(echo "$file" | sed 's/_headless//g')
-  { echo "'problem_id', 'problem_display_name', 'course_display_name', 'problem_text', 'trackevent_hook',
-   'vertical_uri', 'problem_idx', 'sequential_uri', 'vertical_idx', 'chapter_uri', 'sequential_idx',
-   'chapter_idx', 'context'"; cat $file; } > $outf
+  { echo "'problem_id', 'problem_display_name', 'course_display_name', 'problem_text', 'trackevent_hook', 'vertical_uri', 'problem_idx', 'sequential_uri', 'vertical_idx', 'chapter_uri', 'sequential_idx', 'chapter_idx', 'context'"; cat $file; } > $outf
 done
 rm ~/Code/irt/data/raws/*_ProblemMetadata_headless.csv
