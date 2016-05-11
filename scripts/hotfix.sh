3ee4cb49a5ef2d837d7798f6a471c8f6b3fff63c
Alex Kindel <atkindel@stanford.edu>
2016-05-10 17:36:57 -0700

    Remove sed backups after hotfix completes

--- scripts/hotfix.sh
+++ scripts/hotfix.sh
@@ -2,5 +2,6 @@
 # Hotfixes for data issues. To run at end of fetch-raws target.
 
 # Fix problem IDs in courses with CDN containing '.'
-sed -e 's/-MedStats_-/-MedStats.-/g' ./data/raws/Medicine_MedStats._Summer2015_ProblemEvents.csv
-sed -e 's/-QMSE01_-/-QMSE01.-/g' ./data/raws/Engineering_QMSE01._Autumn2015_ProblemEvents.csv
+sed -i 's/-MedStats_-/-MedStats.-/g' ./data/raws/Medicine_MedStats._Summer2015_ProblemEvents.csv
+sed -i 's/-QMSE01_-/-QMSE01.-/g' ./data/raws/Engineering_QMSE01._Autumn2015_ProblemEvents.csv
+rm ./data/raws/*.bak
