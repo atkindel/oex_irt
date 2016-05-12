6383bd144263cd047c942fb6eeab91d4bcb8127c
Alex Kindel <atkindel@stanford.edu>
2016-05-12 11:31:47 -0700

    Adjust header scripts

--- scripts/add_certs_header.sh
+++ scripts/add_certs_header.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for file in ~/Code/irt/data/raws/*_Certificates_headless.csv; do
   outf=$(echo "$file" | sed 's/_headless//g')
-  { echo '"anon_screen_name", "course_display_name", "grade", "created_date", "certificate_granted"'; cat $file; } > $outf
+  { echo '"anon_screen_name","course_display_name","grade","created_date","certificate_granted"'; cat $file; } > $outf
 done
 rm ~/Code/irt/data/raws/*_Certificates_headless.csv
