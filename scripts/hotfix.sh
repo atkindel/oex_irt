11f8eb822fbb7a25ce43271113c6b74bad5cbc4b
Alex Kindel <atkindel@stanford.edu>
2016-05-12 15:44:46 -0700

    Add problematic CDNs to hotfix script

--- scripts/hotfix.sh
+++ scripts/hotfix.sh
@@ -4,4 +4,6 @@
 # Fix problem IDs in courses with CDN containing '.'
 sed -i.bak 's/-MedStats_-/-MedStats.-/g' ./data/raws/Medicine_MedStats._Summer2015_ProblemEvents.csv
 sed -i.bak 's/-QMSE01_-/-QMSE01.-/g' ./data/raws/Engineering_QMSE01._Autumn2015_ProblemEvents.csv
+sed -i.bak 's/-INT_WomensHealth-/-INT.WomensHealth-/g' ./data/raws/GlobalHealth_INT.WomensHealth_July2015_ProblemEvents.csv
+sed -i.bak 's/-SciWrite_-/-SciWrite.-/g' ./data/raws/Medicine_SciWrite._Fall2015_ProblemEvents.csv
 rm ./data/raws/*.bak
