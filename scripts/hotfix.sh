139d74de8c7805637cf9b5c6c877156ab2402361
Alex Kindel <atkindel@stanford.edu>
2016-05-12 11:28:25 -0700

    Add backup extension to hotfix sed

--- scripts/hotfix.sh
+++ scripts/hotfix.sh
@@ -2,6 +2,6 @@
 # Hotfixes for data issues. To run at end of fetch-raws target.
 
 # Fix problem IDs in courses with CDN containing '.'
-sed -i 's/-MedStats_-/-MedStats.-/g' ./data/raws/Medicine_MedStats._Summer2015_ProblemEvents.csv
-sed -i 's/-QMSE01_-/-QMSE01.-/g' ./data/raws/Engineering_QMSE01._Autumn2015_ProblemEvents.csv
+sed -i.bak 's/-MedStats_-/-MedStats.-/g' ./data/raws/Medicine_MedStats._Summer2015_ProblemEvents.csv
+sed -i.bak 's/-QMSE01_-/-QMSE01.-/g' ./data/raws/Engineering_QMSE01._Autumn2015_ProblemEvents.csv
 rm ./data/raws/*.bak
