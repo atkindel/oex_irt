f40f456a7751a8c9a2a9dd5541baf334c8507daf
Alex Kindel <atkindel@stanford.edu>
2016-05-12 15:45:34 -0700

    Filter out problems missing parts data and problems not visible in course

--- src/compute_matrices.py
+++ src/compute_matrices.py
@@ -52,6 +52,9 @@ class ItemMatrixComputer(object):
         self.missing = defaultdict(int)
         self.ignored = defaultdict(int)
 
+        # Indicators for item visibility
+        self.ran_in_course = list()
+
         # Computed data
         self.item_attempts = defaultdict(lambda: defaultdict(ItemAttemptData))
         self.item_timing = defaultdict(lambda: defaultdict(ItemTimingData))
@@ -64,15 +67,28 @@ class ItemMatrixComputer(object):
         self.problemset = set()
 
 
+    def loadProblemDefs(self):
+        '''
+        Read problem definitions to determine if problem was visible in course.
+        '''
+        with open(self.problem_defs, 'rU') as f:
+            problems = csv.DictReader(f)
+            for item in problems:
+                if (item['chapter_idx'] > 0):  # and (~item['staff_only']):
+                    self.ran_in_course.append(item['problem_id'])
+
+
     @staticmethod
     def extractProblemID(raw_id):
         '''
         Extract problem URI from event. Requires a little dark magic.
         Problem URI will be (e.g.) i4x-Engineering-QMSE-02-problem-0764cd7b11ce41f9982c4d0699fd6bd4_2_1
         '''
-        raw_id = raw_id.replace('://', '/').replace('/', '-')
-        problemID = raw_id.replace("input_", "").replace("""%5B%5D""", "")
-        return problemID
+        if "://" in raw_id:
+            return ""  # Bad check event: no parts data.
+        else:
+            problemID = raw_id.replace("input_", "").replace("""%5B%5D""", "")
+            return problemID
 
 
     @staticmethod
@@ -87,6 +103,7 @@ class ItemMatrixComputer(object):
         '''
         Read data from problem events into computer.
         '''
+        self.loadProblemDefs()
         with open(self.problem_events, 'rU') as p:
             prob_events = csv.DictReader(p)
             for row in prob_events:
@@ -105,7 +122,9 @@ class ItemMatrixComputer(object):
                               time=self.convertTime(row['time']))
 
                 # Skip if we didn't get any problemID information (might be '\N' or empty string)
-                if len(event.item) < 3:
+                # Also skip if problemID was not visible in the course.
+                item_base_id = problemID[-36:-4]
+                if len(problemID) < 3 or item_base_id not in self.ran_in_course:
                     self.missing[event.type] += 1  # keep track of what we're losing
                     self.dropped.append(event)
                     continue
@@ -176,6 +195,10 @@ class ItemMatrixComputer(object):
                 item = etype.split('/')[6].replace(";_", "-").replace(":--", "-")
                 timing = self.convertTime(row['time'])
 
+                item_base_id = item[-32:]
+                if item_base_id not in self.ran_in_course:
+                    continue  # Skip if item was not actually visible in the course
+
                 # Since we retrieve browse_events pre-sorted, assume first hit for this learner is earliest view
                 if item not in seen[learner]:
                     # Apply data to all item URIs that match the one we just found
@@ -184,11 +207,16 @@ class ItemMatrixComputer(object):
                                                time_to_first_attempt=self.item_attempts[learner][iuri].first_attempt - timing,
                                                time_to_last_attempt=self.item_attempts[learner][iuri].last_attempt - timing)
                         if calcs.time_to_first_attempt < 0 or calcs.time_to_last_attempt < 0:
-                            self.negative.append(calcs)  # Log this learner-item pair as timing-negative and continue without adding to final matrix
+                            self.negative.append({"item": iuri, "learner": learner})  # Log this learner-item pair as timing-negative and continue without adding to final matrix
+                            del self.item_attempts[learner][iuri]  # Drop data for this learner-item pair
                         else:
                             self.item_timing[learner][iuri] = calcs
                         seen[learner].append(item)  # Keep track of which items we've calculated first views for
 
+        for learner in self.item_timing.keys():
+            if learner not in self.item_attempts.keys():
+                self.dropped.append(self.item_attempts[learner])
+                del self.item_attempts[learner]  # if we got no timing data, log it and drop this learner
 
     def loadcheck(self, outfile):
         '''
@@ -217,10 +245,10 @@ class ItemMatrixComputer(object):
             print("Events ignored:", file=out)
             print(json.dumps(self.ignored, indent=4), file=out)
 
-            print("Bad event log:", file=out)
+            print("Dropped events:", file=out)
             print(json.dumps(self.dropped, indent=4), file=out)
 
-            print("Computed negative:", file=out)
+            print("Computed negative timing:", file=out)
             print(json.dumps(self.negative, indent=4), file=out)
 
 
@@ -234,7 +262,7 @@ class ItemMatrixComputer(object):
             wrt = csv.DictWriter(out, problems, 'NA')
             wrt.writeheader()
             data = self.item_attempts if var in ItemAttemptData._fields else self.item_timing
-            for learner in data:
+            for learner in data.keys():
                 rowdata = {'learner': learner}
                 if not len(data[learner].keys()):
                     continue  # Ignore rows for learners that tried no problems
